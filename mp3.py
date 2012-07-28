c124774ef2ce03cf2fa414083a8a07943ba5ba0a
Christopher Marchfelder <marchfelder@googlemail.com>
2012-07-28 18:59:20 +0200

    Made code a bit saner

--- mp3.py
+++ mp3.py
@@ -1,11 +1,12 @@
 import os
 
+if not os.path.exists('mp3'):
+	os.mkdir('mp3')
+
 for f in os.listdir('.'):
+
 	if '.ogg' in f:
 		newFile = f.replace('.ogg', '.mp3')
-		
-		if not os.path.exists('mp3'):
-			os.mkdir('mp3')
 		
 		print "Converting " + f
 		os.system('sox "' + f + '" "mp3/' + newFile + '"')
