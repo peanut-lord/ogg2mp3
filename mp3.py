efac5143acc92399b5d1b2a2ee176f520959dfff
Christopher Marchfelder <marchfelder@googlemail.com>
2012-07-29 09:40:21 +0200

    Replace if-in with glob

--- mp3.py
+++ mp3.py
@@ -1,12 +1,11 @@
 import os
+import glob
 
 if not os.path.exists('mp3'):
 	os.mkdir('mp3')
 
-for f in os.listdir('.'):
-
-	if '.ogg' in f:
-		newFile = f.replace('.ogg', '.mp3')
-		
-		print "Converting " + f
-		os.system('sox "' + f + '" "mp3/' + newFile + '"')
+for f in glob.glob('*.ogg'):
+	newName = f.replace('.ogg', '.mp3')
+	
+	print "Converting " + f
+	os.system('sox "' + f + '" "mp3/' + newName + '"')
